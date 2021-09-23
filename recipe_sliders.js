37a056a7e49b3549da380908e861c38aa2dddcfa
UnacceptableBehaviour <fernandez.simon@gmail.com>
2021-09-23 12:09:02 +0100

    glider settings experiments

--- recipe_sliders.js
+++ recipe_sliders.js
@@ -14,7 +14,7 @@ window.addEventListener('load',function(){
   });
 
 
-
+  // see 'top ten slider - notes' for anotated setup
 
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   // text cards
@@ -128,8 +128,8 @@ window.addEventListener('load',function(){
   //
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   window._ = new Glider(document.querySelector('#bread-g'), {
-      slidesToShow: 1, //'auto',
-      slidesToScroll: 1,
+      slidesToShow: 2, //'auto',
+      slidesToScroll: 2,
       itemWidth: 150,
       draggable: true,
       scrollLock: false,
@@ -144,32 +144,23 @@ window.addEventListener('load',function(){
               breakpoint: 800,
               settings: {
                   slidesToScroll: 'auto',
-                  itemWidth: 300,
+                  itemWidth: 150,
                   slidesToShow: 'auto',
                   exactWidth: true
               }
           },
           {
-              breakpoint: 700,
-              settings: {
-                  slidesToScroll: 4,
-                  slidesToShow: 4,
-                  dots: false,
-                  arrows: false,
-              }
-          },
-          {
               breakpoint: 600,
               settings: {
-                  slidesToScroll: 3,
-                  slidesToShow: 3
+                  slidesToScroll: 4,
+                  slidesToShow: 4
               }
           },
           {
               breakpoint: 500,
               settings: {
-                  slidesToScroll: 2,
-                  slidesToShow: 2,
+                  slidesToScroll: 3,
+                  slidesToShow: 3,
                   dots: false,
                   arrows: false,
                   scrollLock: true
@@ -344,9 +335,10 @@ window.addEventListener('load',function(){
   //
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   window._ = new Glider(document.querySelector('#sweet-g'), {
-      slidesToShow: 1, //'auto',
-      slidesToScroll: 1,
-      itemWidth: 150,
+      slidesToShow: 'auto',
+      slidesToScroll: 'auto',
+      itemWidth: 240,
+      exactWidth: true,
       draggable: true,
       scrollLock: false,
       dots: '#dots',
@@ -355,90 +347,55 @@ window.addEventListener('load',function(){
           prev: '.glider-prev',
           next: '.glider-next'
       },
-      responsive: [
-          {
-              breakpoint: 800,
-              settings: {
-                  slidesToScroll: 'auto',
-                  itemWidth: 300,
-                  slidesToShow: 'auto',
-                  exactWidth: true
-              }
-          },
-          {
-              breakpoint: 700,
-              settings: {
-                  slidesToScroll: 4,
-                  slidesToShow: 4,
-                  dots: false,
-                  arrows: false,
-              }
-          },
-          {
-              breakpoint: 600,
-              settings: {
-                  slidesToScroll: 3,
-                  slidesToShow: 3
-              }
-          },
-          {
-              breakpoint: 500,
-              settings: {
-                  slidesToScroll: 2,
-                  slidesToShow: 2,
-                  dots: false,
-                  arrows: false,
-                  scrollLock: true
-              }
-          }
-      ]
   });
 
 
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  // top ten slider
+  // top ten slider - notes
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   window._ = new Glider(document.querySelector('#movies-g'), {
-      slidesToShow: 1, //'auto',
-      slidesToScroll: 1,
-      itemWidth: 50,
+      // Mobile first defaults
+      // default settings if no breakpoint in play
+      slidesToShow: 'auto',   //1, //'auto',
+      slidesToScroll: 'auto', //1,
+      itemWidth: 50,          // css debug in chrome say 53.5px - hovering say 150px ??
       draggable: true,
-      dragVelocity: 3.3,     // default 3.3
-      scrollLock: false,   // set to false to turn off snap to movie
-      dots: '#dots',
+      dragVelocity: 3.3,      // default 3.3 - movement of slider relative mouse
+      scrollLock: false,      // set to false to turn off snap to movie/element
+      dots: false,            // '#dots', to show dots
       rewind: true,
-      arrows: {
+      arrows: {               // class tags for arrow buttons
           prev: '.glider-prev',
           next: '.glider-next'
       },
       responsive: [
           {
-              breakpoint: 800,
+              breakpoint: 800,            // setting above 800px
               settings: {
                   slidesToScroll: 'auto',
-                  itemWidth: 300,
-                  slidesToShow: 'auto',
-                  exactWidth: true
+                  itemWidth: 150,        // ignored unless slidesToShow:auto & exactWidth:false
+                  slidesToShow: 5,       // if 'auto' calculated based on itemWidth & viewport
+                  exactWidth: false      // if true ignore slidesToShow setting
               }
           },
           {
-              breakpoint: 700,
+              breakpoint: 700,            // setting above 700px
               settings: {
-                  slidesToScroll: 4,
-                  slidesToShow: 4,
+                  slidesToScroll: 4,      // when button is pressed scroll this no of slides
+                  slidesToShow: 4,        // obvious
                   dots: false,
                   arrows: false,
               }
           },
           {
-              breakpoint: 600,
+              breakpoint: 600,            // setting above 600px
               settings: {
                   slidesToScroll: 3,
                   slidesToShow: 3
               }
           },
           {
-              breakpoint: 500,
+              breakpoint: 500,            // setting above 500px
               settings: {
                   slidesToScroll: 2,
                   slidesToShow: 2,
